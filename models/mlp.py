d21a8c8c7d5ad0bfe2031137eb7d80caf8f56084
dhruvkaul1717 <81750841+dhruvkaul1717@users.noreply.github.com>
2021-07-17 15:01:10 +0100

    basic mlp

---
+++ models/mlp.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+  def __init__(self,inp_dim):
+    super().__init__()
+
+    self.mlp = nn.Sequential(
+        nn.Linear(inp_dim,32),
+        nn.ReLU(),
+        nn.Linear(32,16),
+        nn.ReLU(),
+        nn.Linear(16,8),
+        nn.ReLU(),
+        nn.Linear(8,4),
+        nn.ReLU(),
+        nn.Linear(4,1)
+    )
+
+  def forward(self,x):
+    b = x.size(0)
+    x = x.view(b,-1)
+    return self.mlp(x)
