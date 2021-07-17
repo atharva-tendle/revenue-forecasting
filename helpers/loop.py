d8906cffbfbfcbc516c6ce2a6247eb379e399a21
dhruvkaul1717 <81750841+dhruvkaul1717@users.noreply.github.com>
2021-07-17 16:10:43 +0100

    train + dev loop function

---
+++ helpers/loop.py
@@ -0,0 +1,31 @@
+def loop(train_loader,dev_loader,epochs,optimizer,criterion):
+	for epoch in range(epochs):
+		epoch_loss = 0
+		epoch_loss_dev = 0
+	
+		for idx,data_ in enumerate(train_loader):
+			optimizer.zero_grad()
+
+			# Load data.
+			inp,label = data_
+
+			# Compute output.
+			out = net(inp.float()).squeeze(-1)
+
+			loss = criterion(label.float(),out.float())
+			epoch_loss += loss.item()
+			loss.backward()
+			optimizer.step()
+
+		for idx, data_ in enumerate(dev_loader):
+			with torch.no_grad():
+				inp,label = data_
+				out = net(inp.float()).squeeze(-1)
+
+				loss = criterion(label.float(),out.float())
+				epoch_loss_dev += loss.item()
+
+
+		average_loss = epoch_loss/len(train_loader)
+		average_loss_ = epoch_loss_dev/len(dev_loader)
+		print("train loss: {} -- dev loss: {} for epoch {}".format(average_loss,average_loss_,epoch))
