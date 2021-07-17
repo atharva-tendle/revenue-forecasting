d5bbd0f6cc9ad88ee7c66a2a70c39885f3307c7d
dhruvkaul1717 <81750841+dhruvkaul1717@users.noreply.github.com>
2021-07-17 17:16:21 +0100

    Update loop.py

--- helpers/loop.py
+++ helpers/loop.py
@@ -2,18 +2,18 @@ import torch
 
 
 def loop(model,train_loader,dev_loader,epochs,optimizer,criterion):
-  """
-  A general training and inference loop function.
-  params:
-    - model (nn.Module): the model instantiation being used trained.
-    - train_loader (DataLoader): the training set dataloader.
-    - dev_loader (DataLoader): the dev set dataloader.
-    - epochs (int): number of epochs.
-    - optimizer: optimizer being used to update model parameters.
-    - criterion: loss function being used to train the model. 
-  returns:
-    - prints the training and dev set values of mape, reward, dual objective for each epoch.
-  """
+	"""
+	A general training and inference loop function.
+	params:
+		- model (nn.Module): the model instantiation being used trained.
+		- train_loader (DataLoader): the training set dataloader.
+		- dev_loader (DataLoader): the dev set dataloader.
+		- epochs (int): number of epochs.
+		- optimizer: optimizer being used to update model parameters.
+		- criterion: loss function being used to train the model. 
+	returns:
+		- prints the training and dev set values of mape, reward, dual objective for each epoch.
+	"""
 
 	def iteration(set_,model,loader,criterion,epoch,train=True):
 		epoch_mape = 0.0
