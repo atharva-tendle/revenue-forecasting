38de8af713d93a27fde392ce3056103eeaf1e88d
dhruvkaul1717 <81750841+dhruvkaul1717@users.noreply.github.com>
2021-07-18 15:25:43 +0100

    Update data_process.py

--- data/data_process.py
+++ data/data_process.py
@@ -55,14 +55,14 @@ def data_process_(df,assignments,p,f,covid=True):
     dev_inp_data = []
     dev_lab_data = []
     for id in assignments[clust]:
-      pract_train = df[df.id == id].to_numpy()[:-20]
+      pract_train = df[df.id == id].to_numpy()[:-22]
       train_inp, train_lab = seq_create(normalize(pract_train),p,f)
       training_inp_data.append(train_inp)
       training_lab_data.append(train_lab)
       if covid:
-        pract_dev = df[df.id == id].to_numpy()[-20:]
+        pract_dev = df[df.id == id].to_numpy()[-22:]
       else:
-        pract_dev = df[df.id == id].to_numpy()[-20:-12]
+        pract_dev = df[df.id == id].to_numpy()[-22:-12]
       dev_inp, dev_lab = seq_create(normalize(pract_dev),p,f)
       dev_inp_data.append(dev_inp)
       dev_lab_data.append(dev_lab)
